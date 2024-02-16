f63be192a2aa8d28429c6b66475680c3b3a9cb48
Gaurav Sablok <sablokg@gmail.com>
2024-02-16 14:21:43 +0100

    Create shiny_app.R

---
+++ shiny_app.R
@@ -0,0 +1,6 @@
+#! /usr/bin/R
+# Universitat Potsdam
+# Author Gaurav Sablok
+# date: 2024-2-16
+# a shiny application for the analysis of the metabolic mappings following the genome annotations
+# You can also pass the csv file or the text file containing the metabolic mappings. 
